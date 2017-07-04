eeb92b13c7eff18fdf703ed83aa53f7934de76fe
BlueOrchard <josephrichey@me.com>
2017-07-04 00:28:17 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var merge = require('merge-stream');
+var concat = require('gulp-concat');
+var prefix = require('gulp-autoprefixer');
+var clean = require('gulp-clean-css');
+
+var devCSS = 'dev/css/';
+
+gulp.task('cssPack', function() {
+    var sassFiles,
+        cssFiles,
+        themeInfo;
+
+    sassFiles = gulp.src(devCSS + '*.scss')
+        .pipe(sass());
+
+    cssFiles = gulp.src(devCSS + '*.css');
+
+    return merge(sassFiles, cssFiles)
+           .pipe(concat('style.css'))
+           .pipe(prefix())
+           .pipe(gulp.dest("."));
+});
+
+gulp.task('default', ['cssPack'], function() {
+    gulp.watch(devCSS + '*.css', ['cssPack']);
+    gulp.watch(devCSS + '*.scss', ['cssPack']);
+})
---
+++ readme.txt
@@ -0,0 +1,9 @@
+Here's my Boilerplate WordPress theme!
+
+The theme structural format is inspired by Underscores.me.
+
+Note:
+dev/ is where you should edit CSS and SCSS files.
+Do not edit style.css directly as it will be overwritten if files in dev/ are edited.
+
+template-parts is where post content goes.
