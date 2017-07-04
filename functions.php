4498a8260670e6d43b900f4f112f3b5d645aee13
BlueOrchard <josephrichey@me.com>
2017-07-04 03:02:20 -0400

    Added custom.js file
    Might as well add this for custom Javascript and JQuery.

--- functions.php
+++ functions.php
@@ -61,6 +61,10 @@ function bplate_scripts() {
     wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js", false, null, true);
     wp_enqueue_script('jquery');
 
+	//Enqueue Custom JS File
+	wp_register_script( 'customJS', get_template_directory_uri().'/public/js/custom.js', false, null, true);
+	wp_enqueue_script( 'customJS');
+
     //Enqueue Comment Reply script
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
